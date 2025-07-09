ead8cf5fed8f1f8cf348dc5d44d2a92a7a7f411a
domidomdomz <domz.ciriaco@gmail.com>
2025-07-09 21:32:40 +0800

    Added Manufacturer model

---
+++ Reelkix.BackOffice.Domain/Manufacturers/Manufacturer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reelkix.BackOffice.SharedKernel;
+
+namespace Reelkix.BackOffice.Domain.Manufacturers
+{
+    public class Manufacturer : IAuditable
+    {
+        public Guid Id { get; private set; } // Unique identifier for the manufacturer
+        public string Name { get; private set; } = default!; // Name of the manufacturer, must be set before use
+        public string Description { get; private set; } = default!; // Description of the manufacturer, must be set before use
+
+
+        public DateTime CreatedAt { get; set; } // Timestamp when the manufacturer was created
+        public DateTime UpdatedAt { get; set; } // Timestamp when the manufacturer was last updated
+
+        private Manufacturer() { } // Private constructor for EF Core or other ORM usage
+
+        public Manufacturer(Guid id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            CreatedAt = DateTime.UtcNow; // Set the creation timestamp to the current UTC time
+            UpdatedAt = DateTime.UtcNow; // Set the update timestamp to the current UTC time
+        }
+    }
+}
