e5be29c596566cb5bfe6a512fb86a03a5806c8e2
domidomdomz <domz.ciriaco@gmail.com>
2025-07-09 21:41:09 +0800

    Added ManufacturerConfiguration

---
+++ Reelkix.BackOffice.Persistence/Configurations/ManufacturerConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Reelkix.BackOffice.Domain.Manufacturers;
+
+namespace Reelkix.BackOffice.Persistence.Configurations
+{
+    public class ManufacturerConfiguration : IEntityTypeConfiguration<Manufacturer>
+    {
+        public void Configure(EntityTypeBuilder<Manufacturer> builder)
+        {
+            builder.ToTable("Manufacturers");
+
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(m => m.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(m => m.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(m => m.UpdatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
