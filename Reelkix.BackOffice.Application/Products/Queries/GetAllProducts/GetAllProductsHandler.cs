a0b0e36e29c12f44c464225d8e5cffe4db836844
domidomdomz <domz.ciriaco@gmail.com>
2025-07-11 11:14:38 +0800

    Added CQRS query for Get All Products

---
+++ Reelkix.BackOffice.Application/Products/Queries/GetAllProducts/GetAllProductsHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Reelkix.BackOffice.Application.Products.DTOs;
+using Reelkix.BackOffice.Domain.Products;
+using Reelkix.BackOffice.Persistence.Data;
+
+namespace Reelkix.BackOffice.Application.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsHandler
+    {
+        private readonly ApplicationDbContext _db;
+
+        public GetAllProductsHandler(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<List<ProductDto>?> Handle(CancellationToken cancellationToken)
+        {
+            var products = await _db.Products
+                .Include(p => p.Images)
+                .Include(p => p.Manufacturer)
+                .Select(p => new ProductDto
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                    CostPrice = p.CostPrice,
+                    SellingPrice = p.SellingPrice,
+                    ManufacturerId = p.ManufacturerId,
+                    ManufacturerName = p.Manufacturer.Name,
+                    ImageUrls = p.Images.Select(pi => pi.Url).ToList(),
+                }).ToListAsync();
+
+            if (products is null || !products.Any())
+            {
+                return null; // or throw an exception if you prefer
+            }
+
+            return products;
+        }
+        
+    }
+}
