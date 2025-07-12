1c003adfff827a68db228cabb17fa9fb9720896e
domidomdomz <domz.ciriaco@gmail.com>
2025-07-12 14:40:51 +0800

    Added abstraction of File Upload service to Application layer

---
+++ Reelkix.BackOffice.Application/Common/Interfaces/Storage/IS3Service.cs
@@ -0,0 +1,10 @@
+﻿namespace Reelkix.BackOffice.Application.Common.Interfaces.Storage
+{
+    public interface IS3Service
+    {
+        Task UploadFileAsync(Stream stream, string key, string contentType);
+        string GetPublicUrl(string key);
+
+        // Later: Task DeleteFileAsync(string key);
+    }
+}
