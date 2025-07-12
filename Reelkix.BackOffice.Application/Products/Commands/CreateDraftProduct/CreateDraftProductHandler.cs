ffba947e416b4663df8acb0647e8bce2eeb23495
domidomdomz <domz.ciriaco@gmail.com>
2025-07-12 15:25:29 +0800

    Added Fluent validation to Create Draft Product Handler

--- Reelkix.BackOffice.Application/Products/Commands/CreateDraftProduct/CreateDraftProductHandler.cs
+++ Reelkix.BackOffice.Application/Products/Commands/CreateDraftProduct/CreateDraftProductHandler.cs
@@ -1,19 +1,30 @@
-﻿using Reelkix.BackOffice.Persistence.Data;
+﻿using FluentValidation;
+using Reelkix.BackOffice.Application.Products.Commands.CreateDraftProduct.Validators;
+using Reelkix.BackOffice.Persistence.Data;
 
 namespace Reelkix.BackOffice.Application.Products.Commands.CreateDraftProduct
 {
     public class CreateDraftProductHandler
     {
         private readonly ApplicationDbContext _db;
+        private readonly CreateDraftProductCommandValidator _validator;
 
-        public CreateDraftProductHandler(ApplicationDbContext db)
+        public CreateDraftProductHandler(ApplicationDbContext db, CreateDraftProductCommandValidator validator)
         {
             _db = db ?? throw new ArgumentNullException(nameof(db));
+            _validator = validator;
         }
 
         public async Task<Guid> Handle(CreateDraftProductCommand command, CancellationToken cancellationToken)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
+
+            var validationResult = await _validator.ValidateAsync(command, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
             var draft = new Domain.Products.Product(
                 id: Guid.NewGuid(),
                 name: command.Name,
---
+++ Reelkix.BackOffice.Application/Products/Commands/CreateDraftProduct/Validators/CreateDraftProductCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Reelkix.BackOffice.Application.Products.Commands.CreateDraftProduct.Validators
+{
+    public class CreateDraftProductCommandValidator : AbstractValidator<CreateDraftProductCommand>
+    {
+        public CreateDraftProductCommandValidator()
+        {
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Product name cannot be empty.")
+                .MaximumLength(100).WithMessage("Product name cannot exceed 100 characters.");
+        }
+    }
+}
