28618dcc4f21d336076d1c8c78b433d157f7bf6e
domidomdomz <domz.ciriaco@gmail.com>
2025-07-24 19:21:07 +0800

    Execute app.MapOpenApi() even if not dev

--- Reelkix.BackOffice.API/Program.cs
+++ Reelkix.BackOffice.API/Program.cs
@@ -79,10 +79,8 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.MapOpenApi();
-    }
+    app.MapOpenApi();
+    
     app.MapScalarApiReference();
 
     app.UseMiddleware<ExceptionHandlingMiddleware>();
